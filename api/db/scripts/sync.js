f1f0591f8ab46f85ba995817575c836244279ec6
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-12-15 12:04:56 +0100

    creating database type table

--- api/db/scripts/sync.js
+++ api/db/scripts/sync.js
@@ -5,8 +5,7 @@ const sequelize = require("../db");
 const User = require("../../models/user");
 const Region = require("../../models/region.js");
 const Language = require("../../models/language");
-const sequelize = require("../db.js");
-
+const Type = require('../../models/type.js');
 // create tables
 
 const createTables = async () => {
--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const sequelize = require("../db/db.js");
 
 const Nationality = require("./nationality.js");
@@ -10,72 +10,72 @@ const Skill = require("./skill.js");
 const Project = require("./project.js");
 
 const User = sequelize.define(
-   "user",
-   {
-      UserID: {
-         type: Sequelize.INTEGER,
-         autoIncrement: true,
-         allowNull: false,
-         primaryKey: true,
-      },
-      FirstName: {
-         type: Sequelize.STRING(50),
-         allowNull: false,
-      },
-      LastName: {
-         type: Sequelize.STRING(50),
-         allowNull: false,
-      },
-      Password: {
-         type: Sequelize.STRING(256),
-         allowNull: false,
-      },
-      Email: {
-         type: Sequelize.STRING(50),
-         allowNull: false,
-         unique: true,
-      },
-      NationalityID: {
-         type: Sequelize.INTEGER,
-         allowNull: true,
-      },
-      ProfilePicture: {
-         type: Sequelize.BLOB,
-         allowNull: true,
-      },
-      Bio: {
-         type: Sequelize.STRING(256),
-         allowNull: true,
-      },
-      GithubURL: {
-         type: Sequelize.STRING(2048),
-         allowNull: true,
-      },
-      LinkedinURL: {
-         type: Sequelize.STRING(2048),
-         allowNull: true,
-      },
-      WebsiteURL: {
-         type: Sequelize.STRING(2048),
-         allowNull: true,
-      },
-      ClassID: {
-         type: Sequelize.INTEGER,
-         allowNull: true,
-      },
-      JobTitle: {
-         type: Sequelize.STRING(50),
-         allowNull: true,
-      },
-      RegionID: {
-         type: Sequelize.INTEGER,
-         allowNull: true,
-      },
-   },
-   {
-      tableName: "user",
-      timestamps: false,
-   }
+    "user",
+    {
+        UserID: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        FirstName: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        LastName: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        Password: {
+            type: DataTypes.STRING(256),
+            allowNull: false,
+        },
+        Email: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            unique: true,
+        },
+        NationalityID: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        ProfilePicture: {
+            type: DataTypes.BLOB,
+            allowNull: true,
+        },
+        Bio: {
+            type: DataTypes.STRING(256),
+            allowNull: true,
+        },
+        GithubURL: {
+            type: DataTypes.STRING(2048),
+            allowNull: true,
+        },
+        LinkedinURL: {
+            type: DataTypes.STRING(2048),
+            allowNull: true,
+        },
+        WebsiteURL: {
+            type: DataTypes.STRING(2048),
+            allowNull: true,
+        },
+        ClassID: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        JobTitle: {
+            type: DataTypes.STRING(50),
+            allowNull: true,
+        },
+        RegionID: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "user",
+        timestamps: false,
+    }
 );
 
 /**
@@ -85,49 +85,50 @@ const User = sequelize.define(
  */
 
 User.belongsTo(Nationality, {
-   as: "Nationality",
-   foreignKey: "NationalityID",
+    as: "Nationality",
+    foreignKey: "NationalityID",
 });
 
 User.belongsTo(Class, {
-   as: "Class",
-   foreignKey: "ClassID",
+    as: "Class",
+    foreignKey: "ClassID",
 });
 
 User.belongsTo(Region, {
-   as: "Region",
-   foreignKey: "RegionID",
+    as: "Region",
+    foreignKey: "RegionID",
 });
 
 User.belongsToMany(Language, {
-   as: "Language",
-   through: "UserLanguage",
-   uniqueKey: "UserLanguageID",
-   foreignKey: "UserID",
-   otherKey: "LanguageID",
+    as: "Language",
+    through: "UserLanguage",
+    uniqueKey: "UserLanguageID",
+    foreignKey: "UserID",
+    otherKey: "LanguageID",
 });
 
 User.belongsToMany(Type, {
-   as: "Type",
-   through: "UserType",
-   uniqueKey: "UserTypeID",
-   foreignKey: "UserID",
-   otherKey: "TypeID",
+    as: "Type",
+    through: "UserType",
+    uniqueKey: "UserTypeID",
+    foreignKey: "UserID",
+    otherKey: "TypeID",
 });
 
 User.belongsToMany(Skill, {
-   as: "Skill",
-   through: "UserSkill",
-   uniqueKey: "UserSkillID",
-   foreignKey: "UserID",
-   otherKey: "SkillID",
+    as: "Skill",
+    through: "UserSkill",
+    uniqueKey: "UserSkillID",
+    foreignKey: "UserID",
+    otherKey: "SkillID",
 });
 
 User.belongsToMany(Project, {
-   as: "Project",
-   through: "UserProject",
-   uniqueKey: "UserProjectID",
-   foreignKey: "UserID",
-   otherKey: "ProjectID",
+    as: "Project",
+    through: "UserProject",
+    uniqueKey: "UserProjectID",
+    foreignKey: "UserID",
+    otherKey: "ProjectID",
 });
+
 module.exports = User;
