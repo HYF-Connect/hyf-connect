aac0e6d8dcfb942e09f17b2e09d1f51dbdbffb7b
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-12-15 14:20:24 +0100

    removing try,catch condition

--- api/db/scripts/create-user-type.js
+++ api/db/scripts/create-user-type.js
@@ -1,15 +1,12 @@
 const UserType = require('../../models/user-type.js');
 
 const CreateUserType = async ()=>{
-    try {
-        return await UserType.bulkCreate([
+    
+        const userType = await UserType.bulkCreate([
         {UserTypeID: 1, UserID: 2,TypeID:3},
         {UserTypeID: 2,UserID: 5,TypeID:1}
     ]);
-    
-    } catch (error) {
-        console.log(error);
-    }
+    return userType;
     
     
 };
