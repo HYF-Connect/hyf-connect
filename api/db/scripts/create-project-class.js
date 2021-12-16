44ee22fabbeec617021bbad1af989772c651fdbc
Rayane Silva <rayanej.silva@gmail.com>
2021-12-16 16:22:01 +0100

    Fixed ReferenceError: Cannot access 'projectClass' before initialization

--- api/db/scripts/create-project-class.js
+++ api/db/scripts/create-project-class.js
@@ -1,11 +1,11 @@
-const projectClass = require('../../models/project-class.js');
+const projectClass = require("../../models/project-class.js");
 
 const createProjectClass = async () => {
-    const projectClass = await projectClass.bulkCreate([
-        {ProjectClassID: 1, ClassID: 3, ProjectID: 2},
-        {ProjectClassID: 2, ClassID: 2, ProjectID: 1}
-    ]);
-    return projectClass
+   const projectClasses = await projectClass.bulkCreate([
+      { ProjectClassID: 1, ClassID: 3, ProjectID: 2 },
+      { ProjectClassID: 2, ClassID: 2, ProjectID: 1 },
+   ]);
+   return projectClasses;
 };
 
 createProjectClass();
