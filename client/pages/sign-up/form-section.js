8c90a6bf2fae3baa4006a32f90f5ad8cfd881b7e
Rayane Silva <rayanej.silva@gmail.com>
2021-12-20 12:19:01 +0100

    form section

---
+++ client/pages/sign-up/form-section.js
@@ -0,0 +1,10 @@
+const FormSection = {
+   data: () => ({ message: "Sign up" }),
+   template: `
+    <div class="form">
+      <h1>{{message}}</h1>
+    </div>
+    `,
+};
+
+export default FormSection;
--- client/src/pages/sign-up/sign-up.js
+++ client/src/pages/sign-up/sign-up.js
@@ -1,17 +1,20 @@
 import HeaderComponent from "../../components/header-component.js";
 import FooterComponent from "../../components/footer-component.js";
 import MainSection from "./main-section.js";
+import FormSection from "./form-section.js";
 
 const page = Vue.createApp({
-  components: {
-    HeaderComponent,
-    FooterComponent,
-    MainSection,
-  },
-  template: `
+   components: {
+      HeaderComponent,
+      FooterComponent,
+      MainSection,
+      FormSection,
+   },
+   template: `
       <div class="page">
         <header-component></header-component>
         <main-section></main-section>
+         <form-section></form-section>
         <footer-component></footer-component>
       </div>
     
