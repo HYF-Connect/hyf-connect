445e9ed748de7fe7a8f2f47ec73de0b1569c7908
Rayane Silva <rayanej.silva@gmail.com>
2021-12-20 13:43:54 +0100

    form validation

--- client/pages/sign-up/form-section.js
+++ client/pages/sign-up/form-section.js
@@ -1,9 +1,26 @@
-const FormSection = {
+export const FormSection = {
    data: () => ({ message: "Sign up" }),
    template: `
-    <div class="form">
-      <h1>{{message}}</h1>
-    </div>
+    <form>
+      <h2>Sign Up</h2>
+      <label for="first name">First name</label>
+        <input id="first-name" v-model="first-name" type="text"  required>
+      <label for="last name">Last name</label>
+        <input id="last-name" v-model="last-name" type="text"  required>
+      <label for="email">Email</label>
+        <input id="email" v-model="email" type="email"  required>
+      <label for="password">Password</label>  
+        <input id="password" v-model="password" type="password"  required>    
+      <label for="confirm password">Confirm Password</label> 
+        <input id="password" v-model="password" type="password"  required>
+      <div>
+          <input id="terms" v-model="terms" type="checkbox" required>
+          <label for "terms and conditions">I've read and agree to the terms and conditions</label>
+      </div>
+      <div class="button-account">
+        <button class="submit" type="submit">Create an account</button>
+      </div>
+    </form>
     `,
 };
 
