adf895ce2c67abbd0555468c828db9d4a25c051f
RosaMoran <rosamoran1996@gmail.com>
2021-12-23 00:49:52 +0100

    footer in process

--- client/components/footer-component.js
+++ client/components/footer-component.js
@@ -1,5 +1,16 @@
 const FooterComponent = {
-    template: ``
-  }
-  
-  export default FooterComponent;
+  template: `
+    <div id="footer">
+    <a class="link" href="/">
+    <img alt="HYF_connect logo" src="/public/assets/logo_white_icon.png" width="90"/>
+    <img class="footer_icons" alt="HYF_connect logo" src="/public/assets/logo-github.svg" width="43.08"  height="43.08"/>
+    <img class="footer_icons" alt="HYF_connect logo" src="/public/assets/logo-facebook.svg" width="43.08"  height="43.08"/>
+    <img class="footer_icons" alt="HYF_connect logo" src="/public/assets/logo-instagram.svg" width="43.08"  height="43.08"/>
+    </a>
+    <span class="footer_pp">Privacy Policy | Terms and Condition</span>
+    <span class="footer_connect">© HYF Connect 2021</span>
+    <span class="footer_code">code along with us</span>
+    </div>`,
+};
+
+export default FooterComponent;
