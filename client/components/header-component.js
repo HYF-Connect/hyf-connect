7e062d0e8c415c9f10c82de5d10084866835892a
fritiy <firewynigetahun@gmail>
2021-12-23 06:25:28 -0500

    created new header

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -1,14 +1,20 @@
 const HeaderComponent = {
-  template: `<div class="header">
-  <a class="link" href="/">
-    <img alt="HYF_connect logo" src="/public/assets/logo_pc_full.png" width="90"/>
-  </a>
-  <a class="link" href="/pages/projects/projects.html">projects</a>
-  <a class="link" href="/pages/members/members.html">members</a>
-  <a class="link" href="/pages/about-us/about-us.html">about us</a>
-  <a class="link" href="/pages/contact-us/contact-us.html">contact us</a>
-  <button>Log In</button>
-</div>`
+  template: `<section class="header">
+  <div class="header-container">
+      <div class="header-left">
+      <a href="https:" class="logo">
+      <img src="/client/public/assets/logo_pc_full.png" alt="logo" width="100" height="80"/></a>
+      </div>
+      <div class="header-right">
+      <a class="nav-link" href="/pages/projects/projects.html">projects</a>
+      <a class="nav-link" href="/pages/members/members.html">members</a>
+      <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
+      <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
+      <button class="header-right button" href="link">Sign in</button>
+  
+</div>
+</div>
+</section>`
 }
 
 export default HeaderComponent;
