9bab52c6ed9d2ba0197cbdc88e4206b64a7b92a5
reinaldo <perezrei@gmail.com>
2021-12-23 14:57:49 +0100

    removing benefits from this branch

--- client/pages/homepage/homepage.js
+++ client/pages/homepage/homepage.js
@@ -1,17 +1,14 @@
 import HeaderComponent from "../../components/header-component.js";
 import BannerSection from "./banner-section";
-import BenefitSection from "./benefit-section.js";
 
 const homepage = Vue.createApp({
   components: {
     HeaderComponent,
     BannerSection,
-    BenefitSection
   },
   template: `
   <header-component></header-component>
   <banner-section></banner-section>
-  <benefit-section></benefit-section>
   `,
 }).mount("#homepage");
 
