d8157bc432dd94ba4f5e47a7cb4650895fe7c7c0
Rayane Silva <rayanej.silva@gmail.com>
2021-12-23 19:19:57 +0100

    add main section for the sign-up page

--- client/pages/sign-up/sign-up.js
+++ client/pages/sign-up/sign-up.js
@@ -1,21 +1,20 @@
-import HeaderComponent from "../../components/header-component.js";
+// import HeaderComponent from "../../components/header-component.js";
 // import FooterComponent from "../../components/footer-component.js";
 import MainSection from "./main-section.js";
 import FormSection from "./form-section.js";
 
 const SignUpPage = Vue.createApp({
    components: {
-      HeaderComponent,
+      // HeaderComponent,
       // FooterComponent,
       MainSection,
       FormSection,
    },
    template: `
-      <div class="page">
+      <div class="sign-page">
         <main-section></main-section>
          <form-section></form-section>
       </div>
-    
       `,
 }).mount("#sign-up");
 
