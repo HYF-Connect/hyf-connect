962958fa8bb198f5c05cebcdf081a5b2902f95fb
fritiy <firewynigetahun@gmail>
2021-12-24 05:57:54 -0500

    changed the header

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -1,7 +1,7 @@
 const HeaderComponent = {
   template: `<div class="header-container">
   <div class="brand">
-          <a href="https:" class="logo"><img src="/client/public/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
+          <a href="https:" class="logo"><img src="/public/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
           </div>
           <div class="nav-bar">
               <a class="nav-link" href="/pages/projects/projects.html">projects</a>
