47d6d8e82a4c0f7cec705c11f40e56066e5c9374
fritiy <firewynigetahun@gmail>
2021-12-24 04:58:11 -0500

    created new header-components

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -1,20 +1,16 @@
 const HeaderComponent = {
-  template: `<section class="header">
-  <div class="header-container">
-      <div class="header-left">
-      <a href="https:" class="logo">
-      <img src="/client/public/assets/logo_pc_full.png" alt="logo" width="100" height="80"/></a>
-      </div>
-      <div class="header-right">
-      <a class="nav-link" href="/pages/projects/projects.html">projects</a>
-      <a class="nav-link" href="/pages/members/members.html">members</a>
-      <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
-      <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
-      <button class="header-right button" href="link">Sign in</button>
-  
-</div>
-</div>
-</section>`
+  template: `<div class="header-container">
+  <div class="brand">
+          <a href="https:" class="logo"><img src="/client/public/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
+          </div>
+          <div class="nav-bar">
+              <a class="nav-link" href="/pages/projects/projects.html">projects</a>
+              <a class="nav-link" href="/pages/members/members.html">members</a>
+              <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
+              <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
+          </div>
+          <a class="sign-in-btn" href="/pages/sign-in.html">Sign-in</a>
+      </div>`
 }
 
 export default HeaderComponent;
