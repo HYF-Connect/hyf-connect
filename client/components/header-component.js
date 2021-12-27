80f58df6b639bb1ce45b113e6fb8224eb9efc76a
Rayane Silva <rayanej.silva@gmail.com>
2021-12-27 11:35:04 +0100

    fixed the path to the links in the nav bar

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -1,15 +1,15 @@
 const HeaderComponent = {
    template: `<div class="header-container">
   <div class="brand">
-          <a href="https:" class="logo"><img src="../../public/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
+          <a href="../homepage/homepage.html" class="logo"><img src="../../public/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
           </div>
           <div class="nav-bar">
-              <a class="nav-link" href="/pages/projects/projects.html">projects</a>
-              <a class="nav-link" href="/pages/members/members.html">members</a>
-              <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
-              <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
+              <a class="nav-link" href="../projects/projects.html">projects</a>
+              <a class="nav-link" href="../members/members.html">members</a>
+              <a class="nav-link" href="../about-us/about-us.html">about us</a>
+              <a class="nav-link" href="../contact-us/contact-us.html">contact us</a>
           </div>
-          <a class="sign-in-btn" href="/pages/sign-in.html">Sign-in</a>
+          <a class="sign-in-btn" href="../sign-in/sign-in.html">Sign-in</a>
       </div>`,
 };
 
