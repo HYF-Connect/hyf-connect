f37a87ca6a92b9589d20aa665e3b191817a725c7
Rayane Silva <rayanej.silva@gmail.com>
2021-12-27 20:17:54 +0100

    main section for the sign-up page

--- client/pages/sign-up/main-section.js
+++ client/pages/sign-up/main-section.js
@@ -1,6 +1,12 @@
 const MainSection = {
    template: `
-    <div class="main">
+    <div class = "main__trapezoid">  
+    </div>
+    <div class = "main__text-box"> 
+        <p class = "main__text-right">Welcome!</p>
+        <p class = "main__text-left">sign up here</p>
+        <p class = "main__text-right">to join our</p>
+        <p class = "main__text-left">community</p> 
     </div>
     `,
 };
