05f695456f07a2322712a67aaa10777142ce00f4
reinaldo <perezrei@gmail.com>
2021-12-27 15:58:25 +0100

    creating benefit section html and css

--- client/pages/homepage/benefit-component.js
+++ client/pages/homepage/benefit-component.js
@@ -1,12 +1,23 @@
 const BenefitComponent = {
   props: ['benefit'],
   template: `
-  <div class="benefit">
+  <section class="benefit-section";>  
+  <h2 class="benefit__title">Why Hack Your Future Connect</h2>  
+  <div class="benefit__container">   
     <div class="benefit__card">
-        <div class="benefit__card--avatar">{{benefit.avatar}}</div>
-        <div class="benefit__card--content">{{benefit.content}}</div>
+      <img class="benefit__card--avatar" src="images/key_benefit_first.png" alt="" />
+      <p class="benefit__card--content">sharing knowledge <br />& opportunities</p> 
+    </div>    
+    <div class="benefit__card">
+        <img class="benefit__card--avatar" src="images/key_benefit_second.png" alt="" />
+        <p class="benefit__card--content">build & showcase <br> your portfolio</p> 
+    </div>   
+    <div class="benefit__card">
+        <img class="benefit__card--avatar" src="images/key_benefit_third.png" alt="" />
+        <p class="benefit__card--content">grow together <br>& help each other</p> 
     </div>
-  </div>`
+  </div>
+</section>`
 }
 
 export default BenefitComponent; 
