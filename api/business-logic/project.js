2aeee2c6d690c9f57954415c47ea6a2447b7cdbc
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-12-28 15:24:11 +0100

    editing the responce message

--- api/business-logic/project.js
+++ api/business-logic/project.js
@@ -1,32 +1,33 @@
 const ProjectStore = require("../models/project");
 
 const projectManager = {
-createProject: async ({ Title, Description, GithubURL, WebsiteURL, Thumbnail }) => {
-const newProject = { Title, Description, GithubURL, WebsiteURL, Thumbnail };
-await ProjectStore.create(newProject);
-return newProject;
-},
-getProjectById: async(projectId) =>{
-try {
-    const project = await ProjectStore.findByPk(projectId);
-//findOne({ where: { ProjectID: projectId } });
-   
+  createProject: async ({
+    Title,
+    Description,
+    GithubURL,
+    WebsiteURL,
+    Thumbnail,
+  }) => {
+    const newProject = { Title, Description, GithubURL, WebsiteURL, Thumbnail };
+    await ProjectStore.create(newProject);
+    return newProject;
+  },
+  getProjectById: async (projectId) => {
+    try {
+      const project = await ProjectStore.findByPk(projectId);
 
-if (!project) {
-throw new Error(`Could not find a project with id ${projectId}!`);
-    } 
-    return project;
-} catch (error) {
- console.log(errer)
-}
-
-},
-getAllProjects: async () => {
-const allProjects = await ProjectStore.findAll();
-return allProjects;
-}
-
+      if (!project) {
+        throw new Error(`Could not find a project with id ${projectId}!`);
+      }
+      return project;
+    } catch (error) {
+      console.log(errer);
+    }
+  },
+  getAllProjects: async () => {
+    const allProjects = await ProjectStore.findAll();
+    return allProjects;
+  },
 };
 
 module.exports = projectManager;
-
--- api/controllers/project.js
+++ api/controllers/project.js
@@ -2,55 +2,54 @@ const projectManager = require("../business-logic/project");
 const ProjectStore = require("../models/project");
 
 const projectController = {
-    get: async (req, res) => {
+  get: async (req, res) => {
     try {
-        const allProjects = await projectManager.getAllProjects();
-        res.send(JSON.stringify(allProjects, null, 2));
+      const allProjects = await projectManager.getAllProjects();
+      res.send(JSON.stringify(allProjects, null, 2));
     } catch (error) {
-    res.status(500).send(error);
+      res.status(500).send({ message: error });
     }
-},
+  },
 
-getProjectById: async (req, res) => {
+  getProjectById: async (req, res) => {
     try {
-        const projectId = req.params.projectId;
-        const project = await projectManager.getProjectById(projectId);
-        res.send(JSON.stringify(project));
+      const projectId = req.params.projectId;
+      const project = await projectManager.getProjectById(projectId);
+      res.send(JSON.stringify(project));
     } catch (error) {
-        res.status(500).send(error.message);
+      res.status(500).send({ message: error.message });
     }
-},
+  },
 
-post: async (req, res) => {
+  post: async (req, res) => {
     try {
-        const { Title, Description } = req.body;
-        if (!Title || !Description ) {
+      const { Title, Description } = req.body;
+      if (!Title || !Description) {
         return res.status(400).json({
-            message:
-                "Please, enter the project's title and description",
+          message: "Please, enter the project's title and description",
         });
-        }
+      }
+      const projectAlreadyExists = await ProjectStore.findOne({
+        where: { Title },
+      });
+      if (projectAlreadyExists) {
+        return res
+          .status(409)
+          .json({ message: "This project title is already being used" });
+      }
 
-
-        const projectAlreadyExists = await ProjectStore.findOne ({ where: { ProjectID }});
-        if (projectAlreadyExists) {
-            return res
-                .status(409)
-                    .json({ message: "This project title is already being used" });
-        }
-
-        const project = await projectManager.createProject({
-                    Title,
-                    Description,
-                });
-            res.status(200).json(
-                `Congratulation ${project.Title}, is created!`
-            );
+      const project = await projectManager.createProject({
+        Title,
+        Description,
+      });
+      res
+        .status(200)
+        .json({ message: `Congratulation ${project.Title}, is created!` });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error.message });
+      console.log(error);
+      res.status(500).json({ message: error.message });
     }
-    },
+  },
 };
 
 module.exports = projectController;
