fd11c6d84630836f4eb0182f640994a04b623a12
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-12-28 19:22:13 +0100

    resolving the last conflict

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-=======
+
 // const userManager = require("../business-logic/user");
->>>>>>> 07382f19270eb6947ba59a566030e5af614aea9a
 
 // require routes files
 const userRoutes = require("./user");
 const loginRoutes = require("./login.js");
 const AuthRoutes = require("./authentication-route");
-const projectRoutes = require ("./project");
+const projectRoutes = require("./project");
 // use them with this router
 
 router.use(AuthRoutes);
 router.use("/users", userRoutes);
 router.use("/users", loginRoutes);
-router.use("/projects",projectRoutes);
+router.use("/projects", projectRoutes);
 // this route was added for testing the middleware
 router.get("/auth", (req, res) => {
-   console.log(req.loggedInUser);
-   res.json({ mes: "ok" });
+  console.log(req.loggedInUser);
+  res.json({ mes: "ok" });
 });
 // export the routes
 module.exports = router;
