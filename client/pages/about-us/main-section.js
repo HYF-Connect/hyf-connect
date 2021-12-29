6aa540df4f4c694a833def3c08e577c65d5fc912
fritiy <firewynigetahun@gmail>
2021-12-29 16:34:14 -0500

    Created the about us page

--- client/pages/about-us/main-section.js
+++ client/pages/about-us/main-section.js
@@ -1,9 +1,147 @@
 const MainSection = {
   data: () => ({ message: "About Us" }),
   template: `
-    <div class="main">
-      <h1>{{message}}</h1>
-    </div>
+  <div class="about-us-container">
+  <h1>About us</h1>
+
+</div>
+<div class="about-us">
+    <div class = "About-hyf"> 
+        <p><strong>HackYourFuture Belgium </strong>is an initiative of Open Knowledge Belgium.
+    It is a coding school to support those who have limited access to education and labor market
+    Operated in Brussels and Antwerp. <br> <br>
+    <strong>HackYourFuture aims</strong> to enable 
+    refugees and disadvantaged people to build digital skills for a career in web development, 
+    facilitate the integration of newcomers, and address the shortage of qualified workforce in the IT sector.</p>
+    <br>
+    <a class="About-visit-website" href="">visit website</a>
+</div>
+<div class="About-hyf-team">
+<img src="/images/homepage/hyf-team.jpeg"  alt="pic" width="650" height="400"></a>
+</div>
+<div class="About-title-1">
+<h2> what is hyf-connect?</h2>
+</div>
+<div class="About-hyf-connect">
+<p><strong>HackYourFuture Connect</strong> is the final project developed by students from #class15.<br> <br>
+<strong>HYC Connect</strong> is a platform where all students, alumni, and coaches can gather together, 
+continuing the HYF journey. In this platform, everyone can share their knowledge about coding world, 
+share their profile for new opportunities to grow. 
+Our mission is to connect, to share, and to grow together as one big HackYourFuture Community</p>
+
+<a class="About-visit-project" href="">See project</a>
+</div> 
+<div class="About-hyf-connect2">
+<img src="/assets/logo_pc_full.png"  alt="logo" width="700" height="650">
+</div>
+</div>
+
+<div class="About-title">
+<h4>people behind this project</h4>
+</div>
+<div class="About-main">
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/MoaminA.png">
+</div>
+<div class="caption">
+<h3>Moamine</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/InggritEN.jpg">
+</div>
+<div class="caption">
+<h3>Inggrit</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/LauraR.jpg">
+</div>
+<div class="caption">
+<h3>Laura</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/RosaM.jpg">
+</div>
+<div class="caption">
+<h3>Rosa</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/HajirA.jpg">
+</div>
+<div class="caption">
+<h3>Hajir</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/RayaneS.jpg">
+</div>
+<div class="caption">
+<h3>Rayane</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/ReinaldoP.jpg">
+</div>
+<div class="caption">
+<h3>Reinaldo</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/Firewyni.jpg">
+</div>
+<div class="caption">
+<h3>Frewyni</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/MariaP.jpeg">
+</div>
+<div class="caption">
+<h3>MariaP</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+<div class="profile-card">
+<div class="img">
+<img src="/images/members/MoaminA.png">
+</div>
+<div class="caption">
+<h3>Moamine</h3>
+<p>Full Stack Developer</p>
+</div>
+</div>
+
+</div>
+<div class="About-last-container">
+
+</div>
     `,
 };
 
