bee84f338e0056ff2f4d6541671c256065e61993
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-12-29 06:11:48 +0100

    members page styling

---
+++ api/controllers/project.js
@@ -0,0 +1,55 @@
+const projectManager = require("../business-logic/project");
+const ProjectStore = require("../models/project");
+
+const projectController = {
+  get: async (req, res) => {
+    try {
+      const allProjects = await projectManager.getAllProjects();
+      res.send(JSON.stringify(allProjects, null, 2));
+    } catch (error) {
+      res.status(500).send({ message: error });
+    }
+  },
+
+  getProjectById: async (req, res) => {
+    try {
+      const projectId = req.params.projectId;
+      const project = await projectManager.getProjectById(projectId);
+      res.send(JSON.stringify(project));
+
+      if (!project) {
+        throw new Error(`Could not find a project with id ${projectId}!`);
+      }
+      return project;
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  },
+
+  post: async (req, res) => {
+    try {
+      const { Title, Description, GithubURL, WebsiteURL, Thumbnail } = req.body;
+      if (!Title || !Description) {
+        return res.status(400).json({
+          message: "Please, enter the project's title and description",
+        });
+      }
+
+      const project = await projectManager.createProject({
+        Title,
+        Description,
+        GithubURL,
+        WebsiteURL,
+        Thumbnail,
+      });
+      res
+        .status(200)
+        .json({ message: `Congratulation ${project.Title}, is created!` });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+};
+
+module.exports = projectController;
--- client/pages/members/banner-section.js
+++ client/pages/members/banner-section.js
@@ -1,15 +1,14 @@
 export const bannerSection = {
   template: `
     <section class="members-banner">
-    <div class= "members-banner__left">
-      <img class="members-banner__left--image" src="../../images/members-page/members-list-banner.svg" alt="banner-img">
+    <div class= "members-banner" style="background-color: rgba(47, 25, 95, 1);padding-bottom: 85px;">
+        <img class="members-banner__image" src="../../images/members-page/members-list-banner.svg" alt="banner-img">
+        <div class= "members-banner__text">
+            <h3 class= "members-banner__header">check out </h3>
+            <h2 class="members-banner__header">our members </h2>
+        </div>
     </div>
-    <div class= "members-banner__right">
-      <h3 class= "members-banner__right--title">check out </h3>
-      <h2 class="members-banner__right--title">our members </h2>;
-
-    </div>
-  </section>
+    </section>
 
 `,
 };
