eeac569afd458cb08442f890516a5d714b3c89a9
reinaldo <perezrei@gmail.com>
2021-12-29 13:47:52 +0100

    adding footer to homepage.js

--- client/pages/homepage/homepage.js
+++ client/pages/homepage/homepage.js
@@ -2,19 +2,22 @@ import HeaderComponent from "../../components/header-component.js";
 import BannerSection from "./banner-section.js";
 import BenefitSection from "./benefit-section.js";
 import TestimonialSection from "./testimonial-section.js";
+import FooterComponent from "/components/footer-component.js";
 
 const homepage = Vue.createApp({
   components: {
     HeaderComponent,
     BannerSection,
     BenefitSection,
-    TestimonialSection
+    TestimonialSection,
+    FooterComponent
   },
   template: `
   <header-component></header-component>
   <banner-section></banner-section>
   <benefit-section></benefit-section>
   <testimonial-section></testimonial-section>
+  <footer-component></footer-component>
   `,
 }).mount("#homepage");
 
