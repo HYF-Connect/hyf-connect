19cb144ff8e48cbf306c0726fc0813fbb2b0f134
reinaldo <perezrei@gmail.com>
2021-12-30 11:11:03 +0100

    adding carousel component

--- client/pages/homepage/homepage.js
+++ client/pages/homepage/homepage.js
@@ -3,6 +3,7 @@ import BannerSection from "./banner-section.js";
 import BenefitSection from "./benefit-section.js";
 import TestimonialSection from "./testimonial-section.js";
 import FooterComponent from "/components/footer-component.js";
+import CarouselComponent from "./carousel-component.js";
 
 const homepage = Vue.createApp({
   components: {
@@ -10,12 +11,14 @@ const homepage = Vue.createApp({
     BannerSection,
     BenefitSection,
     TestimonialSection,
+    CarouselComponent,
     FooterComponent
   },
   template: `
   <header-component></header-component>
   <banner-section></banner-section>
   <benefit-section></benefit-section>
+  <carousel-component></carousel-component>
   <testimonial-section></testimonial-section>
   <footer-component></footer-component>
   `,
