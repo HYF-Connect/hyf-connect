6e196d574468e29e37a0e84f30ba9da3a9527d5c
Inggrit E Novaleta <inggriteunikenovaleta@gmail.com>
2021-12-30 10:21:05 +0100

    added html template for add-new-project-page

--- client/pages/add-new-project/main-section.js
+++ client/pages/add-new-project/main-section.js
@@ -1,9 +1,40 @@
 const MainSection = {
   data: () => ({ message: "Add new project" }),
   template: `
-    <div class="main">
-      <h1>{{message}}</h1>
+  <div class="addnewproject__banner">
+  <h1>Add your new project here!</h1>
+</div>
+<div class="addnewproject__wrapper">
+  <div class="addnewproject__wrapper--form">
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Project Title</label>
+      <input type="text" class="input" placeholder="Your project's title" style="width: 400px">
     </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Project URL</label>
+      <input type="url" class="input" placeholder="url of the project" style="width: 400px">
+    </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Github Repo</label>
+      <input type="url" class="input" placeholder="Github repo of the project" style="width: 400px">
+    </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Description</label>
+      <textarea class="textarea" placeholder="Describe your project" style="width: 400px; height:50px"></textarea>
+    </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Team Members</label>
+      <input type="text" class="input" placeholder="Tag your team member of this project starting with '@' " style="width: 400px">
+    </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <label>Add Thumbnail</label>
+      <input type="file" class="input" accept="Image/*" style="width: 400px;">
+    </div>
+    <div class="addnewproject__wrapper--inputfield">
+      <input type="submit" value="save project" class="addnewproject__btn">
+    </div>
+  </div>
+</div>
     `,
 };
 
