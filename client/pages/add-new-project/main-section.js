ce73405472ac6177b0cfb537f646b7d783294996
Inggrit E Novaleta <inggriteunikenovaleta@gmail.com>
2022-01-03 14:20:17 +0100

    add-new-project-page commit

--- client/pages/add-new-project/main-section.js
+++ client/pages/add-new-project/main-section.js
@@ -4,38 +4,68 @@ const MainSection = {
   <div class="addnewproject__banner">
   <h1>Add your new project here!</h1>
 </div>
-<div class="addnewproject__wrapper">
-  <div class="addnewproject__wrapper--form">
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Project Title</label>
-      <input type="text" class="input" placeholder="Your project's title" style="width: 400px">
+<div class="addnewproject__container">
+  <form class="addnewproject__form">
+    <label class="addnewproject__form--label">Project Title</label>
+      <input class="addnewproject__form--input" type="text" placeholder="Your project's title" required v-model="projectTitle" style="width: 400px">
+    <label class="addnewproject__form--label">Project URL</label>
+      <input class="addnewproject__form--input" type="url" placeholder="example@example.com" required v-model="websiteUrl" style="width: 400px">
+    <label class="addnewproject__form--label">Github Repo</label>
+      <input class="addnewproject__form--input" type="url" placeholder="example@github.com" required v-model="githubRepo" style="width: 400px">
+    <label class="addnewproject__form--label">Description</label>
+      <textarea class="addnewproject__form--textarea" placeholder="Describe your project" required v-model="projectDescription" style="width: 400px; height:50px"></textarea>
+    <label class="addnewproject__form--label">Team Members</label>
+      <!-- <input class="addnewproject__form--input" type="text" placeholder="Tag your team member of this project starting with '@' " style="width: 400px"> -->
+      <select class="select" multiple data-mdb-filter="true">
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+        <option value="5">Five</option>
+        <option value="6">Six</option>
+        <option value="7">Seven</option>
+        <option value="8">Eight</option>
+        <option value="9">Nine</option>
+        <option value="10">Ten</option>
+      </select>
+    <label class="addnewproject__form--label">Add Thumbnail</label>
+      <input class="addnewproject__form--input type="file" accept="Image/*" style="width: 400px;">
+    <button class= "addnewproject__form--btn" v-on:click="handleSubmit">save project</button>
     </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Project URL</label>
-      <input type="url" class="input" placeholder="url of the project" style="width: 400px">
-    </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Github Repo</label>
-      <input type="url" class="input" placeholder="Github repo of the project" style="width: 400px">
-    </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Description</label>
-      <textarea class="textarea" placeholder="Describe your project" style="width: 400px; height:50px"></textarea>
-    </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Team Members</label>
-      <input type="text" class="input" placeholder="Tag your team member of this project starting with '@' " style="width: 400px">
-    </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <label>Add Thumbnail</label>
-      <input type="file" class="input" accept="Image/*" style="width: 400px;">
-    </div>
-    <div class="addnewproject__wrapper--inputfield">
-      <input type="submit" value="save project" class="addnewproject__btn">
-    </div>
-  </div>
+  </form>
 </div>
     `,
+    data() {
+      return {
+          projectTitle: "",
+          websiteUrl: "",
+          githubRepo: "",
+          projectDescription:"",
+      };
+},
+
+methods: {
+  async handleSubmit() {
+
+      try {
+          const result = await loginUser(
+          this.email,
+          this.password,
+      );
+      localStorage.setItem("token", result.token) ;
+      localStorage.setItem('username', result.userName);
+      this.passwordError = "";
+      this.password = "";
+      this.success = true;
+      setTimeout(() => (window.location.href = "/pages/homepage/homepage.html"), 500);
+      } catch (error) {
+      this.passwordError = "Invalid email or password";
+      this.success = false;
+      console.log("error from login", error);
+      }
+  },  
+
+},
 };
 
 export default MainSection;
