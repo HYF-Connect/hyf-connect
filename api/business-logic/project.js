23d58eefe786b23382df0c1e2e2e9bceb36b6dc6
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2022-01-03 17:45:48 +0100

    creating project juction tables with editing and deleteing fileds

--- api/business-logic/project.js
+++ api/business-logic/project.js
@@ -1,31 +1,105 @@
 const ProjectStore = require("../models/project");
+const ProjectSkillStore = require("../models/project-skill.js");
+const ProjectClassStore = require("../models/project-class.js");
+const UserProjectStore = require("../models/user-project.js");
 
 const projectManager = {
-   createProject: async ({
+  createProject: async ({
+    Title,
+    Description,
+    GithubURL,
+    WebsiteURL,
+    Thumbnail,
+  }) => {
+    const newProject = {
       Title,
       Description,
       GithubURL,
       WebsiteURL,
       Thumbnail,
-   }) => {
-      const newProject = {
-         Title,
-         Description,
-         GithubURL,
-         WebsiteURL,
-         Thumbnail,
-      };
-      await ProjectStore.create(newProject);
-      return newProject;
-   },
-   getProjectById: async (projectId) => {
-      const project = await ProjectStore.findByPk(projectId);
-      return project;
-   },
-   getAllProjects: async () => {
-      const allProjects = await ProjectStore.findAll();
-      return allProjects;
-   },
+    };
+    await ProjectStore.create(newProject);
+    return newProject;
+  },
+  createProjectSkill: async ({ ProjectID, SkillID }) => {
+    const createdProjectSkill = await ProjectSkillStore.create({
+      ProjectID: ProjectID,
+      SkillID: SkillID,
+    });
+    return createdProjectSkill;
+  },
+  createProjectClass: async ({ ProjectID, ClassID }) => {
+    const createdProjectClass = await ProjectClassStore.create({
+      ProjectID,
+      ClassID,
+    });
+    return createdProjectClass;
+  },
+  createUserProject: async ({ UserID, ProjectID }) => {
+    const createdUserProject = await UserProjectStore.create({
+      UserID: UserID,
+      ProjectID: ProjectID,
+    });
+    return createdUserProject;
+  },
+  updateProject: async ({
+    ProjectID,
+    Title,
+    Description,
+    GithubURL,
+    WebsiteURL,
+    Thumbnail,
+    User,
+    Class,
+    Skill,
+  }) => {
+    const projectById = await ProjectStore.findOne({
+      where: { ProjectID: ProjectID },
+    });
+    const updatedProfile = await projectById.update({
+      Title,
+      Description,
+      GithubURL,
+      WebsiteURL,
+      Thumbnail,
+      UserID: User,
+      ClassID: Class,
+      SkillID: Skill,
+    });
+    return updatedProfile;
+  },
+  deleteProject: async ({ ProjectID }) => {
+    await ProjectStore.destroy({
+      where: { ProjectID: ProjectID },
+    });
+    return true;
+  },
+  deleteProjectSkill: async ({ ProjectID, SkillID }) => {
+    await ProjectSkillStore.destroy({
+      where: { ProjectID: ProjectID, SkillID: SkillID },
+    });
+    return true;
+  },
+  deleteProjectClass: async ({ ProjectID, ClassID }) => {
+    await ProjectSkillStore.destroy({
+      where: { ProjectID: ProjectID, ClassID: ClassID },
+    });
+    return true;
+  },
+  deleteUserProject: async ({ UserID, ProjectID }) => {
+    await UserProjectStore.destroy({
+      where: { ProjectID: ProjectID, UserID: UserID },
+    });
+    return true;
+  },
+  getProjectById: async (projectId) => {
+    const project = await ProjectStore.findByPk(projectId);
+    return project;
+  },
+  getAllProjects: async () => {
+    const allProjects = await ProjectStore.findAll();
+    return allProjects;
+  },
 };
 
 module.exports = projectManager;
--- api/controllers/project.js
+++ api/controllers/project.js
@@ -1,5 +1,4 @@
 const projectManager = require("../business-logic/project");
-const ProjectStore = require("../models/project");
 
 const projectController = {
   get: async (req, res) => {
@@ -40,6 +39,157 @@ const projectController = {
       res
         .status(200)
         .json({ message: `Congratulation ${project.Title}, is created!` });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  postSkill: async (req, res) => {
+    try {
+      const { SkillID } = req.body;
+      const projectID = req.params.projectId;
+      const projectSkill = await projectManager.createProjectSkill({
+        ProjectID: projectID,
+        SkillID: SkillID,
+      });
+      res.status(200).json(projectSkill);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  postClass: async (req, res) => {
+    try {
+      const { ClassID } = req.body;
+      const projectID = req.params.projectId;
+      const projectClass = await projectManager.createProjectClass({
+        ProjectID: projectID,
+        ClassID: ClassID,
+      });
+      res.status(200).json(projectClass);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  postUser: async (req, res) => {
+    try {
+      const { UserID } = req.body;
+      const projectID = req.params.projectId;
+      const userProject = await projectManager.createUserProject({
+        ProjectID: projectID,
+        UserID: UserID,
+      });
+      res.status(200).json(userProject);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  putProject: async (req, res) => {
+    try {
+      const {
+        Title,
+        Description,
+        GithubURL,
+        WebsiteURL,
+        Thumbnail,
+        User,
+        Class,
+        Skill,
+      } = req.body;
+      const projectId = req.params.projectId;
+      const project = await projectManager.updateProject({
+        ProjectID: projectId,
+        Title,
+        Description,
+        GithubURL,
+        WebsiteURL,
+        Thumbnail,
+        UserID: User,
+        ClassID: Class,
+        SkillID: Skill,
+      });
+      res
+        .status(200)
+        .json(
+          `Congratulation ${project.Title}, has been successfully updated!`
+        );
+    } catch (error) {
+      res.status(400).json({
+        message: error.message,
+      });
+    }
+  },
+  deleteProject: async (req, res) => {
+    try {
+      const { projectId } = req.params;
+      await projectManager.deleteProject({
+        ProjectID: projectId,
+      });
+      res.status(200).json({
+        message: `project with id ${projectId} was successfully deleted!`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteSkill: async (req, res) => {
+    try {
+      const { skillId, projectId } = req.params;
+      await projectManager.deleteProjectSkill({
+        ProjectID: projectId,
+        SkillID: skillId,
+      });
+      res.status(200).json({
+        message: `Skill with id ${skillId} was successfully deleted!`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteClass: async (req, res) => {
+    try {
+      const { classId, projectId } = req.params;
+      await userManager.deleteProjectClass({
+        ProjectID: projectId,
+        ClassID: classId,
+      });
+      res.status(200).json({
+        message: `Class with id ${classId} was successfully deleted!`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteClass: async (req, res) => {
+    try {
+      const { classId, projectId } = req.params;
+      await userManager.deleteProjectClass({
+        ProjectID: projectId,
+        ClassID: classId,
+      });
+      res.status(200).json({
+        message: `Class with id ${classId} was successfully deleted!`,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const { userId, projectId } = req.params;
+      await userManager.deleteUserProject({
+        ProjectID: projectId,
+        UserID: userId,
+      });
+      res.status(200).json({
+        message: `User with id ${userId} was successfully deleted!`,
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: error.message });
