6523849cddff1607b1ac31429507bad45fbd0cf5
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2022-01-03 15:18:29 +0100

    starting the connection to the backend

--- client/src/data-access/api-calls/calls.js
+++ client/src/data-access/api-calls/calls.js
@@ -1,60 +1,70 @@
 export const performFetch = async (path) => {
-   const URL = `${window.location.origin}/api/${path}`;
+  const URL = `${window.location.origin}/api/${path}`;
 
-   const encodedURL = encodeURI(URL);
-   const response = await fetch(encodedURL, {
-      method: "GET",
-      headers: {
-         'Content-Type': 'application/json',
-         Authorization: localStorage.getItem("token") === undefined ? "" : `Bearer ${localStorage.getItem("token")}`
-      },
-   });
-   if (!response.ok) {
-      console.error(`HTTP error! status: ${response.status}\n-> ${URL}`);
-      const data = await response.json();
-      throw new Error(data.message);
-   }
-   const data = await response.json();
-   
-   return data;
+  const encodedURL = encodeURI(URL);
+  const response = await fetch(encodedURL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        localStorage.getItem("token") === undefined
+          ? ""
+          : `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  if (!response.ok) {
+    console.error(`HTTP error! status: ${response.status}\n-> ${URL}`);
+    const data = await response.json();
+    throw new Error(data.message);
+  }
+  const data = await response.json();
+
+  return data;
 };
 
 export const performPost = async (path, body) => {
-   console.log(JSON.stringify(body));
-   const URL = `${window.location.origin}/api/${path}`;
-   const encodedURL = encodeURI(URL);
-   const response = await fetch(encodedURL, {
-      method: "POST",
-      headers: {
-         'Content-Type': 'application/json',
-         Authorization:
-         localStorage.getItem("token") === undefined ? "" : `Bearer ${localStorage.getItem("token")}`
-      },
-      body: JSON.stringify(body),
-   });
-   if (!response.ok) {
-      console.error(`HTTP error! status: ${response.status}\n-> ${URL}`);
-      const data = await response.json();
-      throw new Error(data.message);
-   }
-   const data = await response.json();
-   return data;
+  console.log(JSON.stringify(body));
+  const URL = `${window.location.origin}/api/${path}`;
+  const encodedURL = encodeURI(URL);
+  const response = await fetch(encodedURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        localStorage.getItem("token") === undefined
+          ? ""
+          : `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    console.error(`HTTP error! status: ${response.status}\n-> ${URL}`);
+    const data = await response.json();
+    throw new Error(data.message);
+  }
+  const data = await response.json();
+  return data;
 };
 export const registerUser = async (FirstName, LastName, Email, Password) => {
-   return await performPost("users/register", {
-      FirstName,
-      LastName,
-      Email,
-      Password,
-   });
+  return await performPost("users/register", {
+    FirstName,
+    LastName,
+    Email,
+    Password,
+  });
 };
 // login into the app
 export const loginUser = async (Email, Password) => {
-   return await performPost("users/login", {
-      Email,
-      Password,
-   });
-   
+  return await performPost("users/login", {
+    Email,
+    Password,
+  });
 };
-
-
+// fetching all users
+export const fetchUsers = async () => {
+  return await performFetch("users", {
+    FirstName,
+    LastName,
+    JobTitle,
+  });
+};
