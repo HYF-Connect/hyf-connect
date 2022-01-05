1dedc402d24fafe18a1286faef9c2480b7092d1d
RosaMoran <rosamoran1996@gmail.com>
2022-01-05 17:50:42 +0100

    process

--- client/pages/contact-us/form-section.js
+++ client/pages/contact-us/form-section.js
@@ -2,8 +2,11 @@
 export const FormSection = {
   template: `
   <div id="section-contact-template" class="section">
-      <form method="post" action="/contact#contact_form" id="contact__form" accept-charset="UTF-8" class="contact-form">
-        <input type="hidden" name="form_type" value="contact">
+      <form method="post" action="/contact#contact_form" id="contact__form" accept-charset="UTF-8" class="contact-form" @submit.prevent="handleSubmit">
+      <div class="alert alert-danger" role="alert" v-if="messageError">
+        {{ messageError }}
+        </div> 
+      <input type="hidden" name="form_type" value="contact">
         <input type="hidden" name="utf8" value="✓">
         <div class="image1">
           <img class="rectangle" alt="rectangle" src="/images/contact-us/Rectangle 740.png"/>
@@ -15,48 +18,23 @@ export const FormSection = {
           <p class="contact__text4"> suggestion ? </p>
           <p class="contact__text5">contact us!</p>
       </div>
-          <div class="img">
-            <img class="image2"  src="/images/contact-us/Screenshot_2021-12-08.png"/>
-          </div>
           <div class="contact__columns">
             <br class="clear">
             <div class="three columns alpha">
-              <label for="contactFormName">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">Name</font>
-                </font>
-                <span class="white">
-                  <font style="vertical-align: inherit;">
-                    <font style="vertical-align: inherit;">*</font>
-                  </font>
-                </span>
-              </label>
-              <input type="text" id="contactFormName" name="contact" placeholder="name" autocapitalize="words" value required="required">
-                <div pseudo="-webkit-input-placeholder" id="placeholder" style="display: block !important;">Name</div>
+              <input type="text" id="contactFormName" name="contact" placeholder="Name" autocapitalize="words" value required="required">
               </input>
             </div>
-          <div class="three columns omega">
-            <label for="contactForEmail">Email
-              <span class="white">*</span>
-            </label>
-            <input type="email" id="contactFormEmail" name="contact[email]" placeholder="Email" autocorrect="off" autocapitalize="off" value required="required">
-            
-              <div pseudo="-webkit-input-placeholder" id="placeholder" style="display: block !important;">Email
-              </div>
-            </input>
-          </div>
-          <label for="contactFormMessage">Message
-            <span class="white">*
-            </span>
-          </label>
-          <textarea rows="5" id="contactFormMessage" name="contact[body]" placeholder="Message" required="required">
-            <div pseudo="-webkit-input-placeholder" id="placeholder" style="display: block !important;">Message
+            <div class="three columns omega">
+            <input type="email" id="contactFormEmail" name="contact[email]" placeholder="Email" autocorrect="off" autocapitalize="off" value required="required">         </input>
             </div>
-          </textarea>
-          <input type="submit" class="submit action_button" value="submit">submit</input>
-        </div>
+            <textarea  class="message" rows="5" cols="25.5"  placeholder="Message" required="required"></textarea>
+            <button type="submit" class="submit action_button" value="submit">submit</button>
+          </div>
         <br class="clear">
         <br class="clear">
+        <div class="img__left">
+        <img class="image2"  src="/images/contact-us/Screenshot_2021-12-08.png"/>
+      </div>
       </form>
   <div >
     `,
