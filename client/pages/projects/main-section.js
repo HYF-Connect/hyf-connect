9189dde15a6e9aa9ecffb54d0fcd694f63befb82
reinaldo <perezrei@gmail.com>
2022-01-06 12:16:52 +0100

    small changes

--- client/pages/projects/main-section.js
+++ client/pages/projects/main-section.js
@@ -22,7 +22,9 @@ const MainSection = {
     return {
       projects: [
         { thumbnail: "https://image.ibb.co/gbiAow/project.jpg", github: "https://github.com/HYF-Connect/hyf-connect", website: "https://hackyourfuture.be/", title: "Project Title", description: "Project description. Maximum length: 40 words. Lorem ipsum, dolor sit amet consectetur adipisicing elit. At non earum officia porro quae, modi ex quidem, consectetur architecto rem beatae illum sequi quis laudantium repudiandae ipsum repellat veniam eaque consequatur dolorem provident dolorum.", members: "Here goes all the names of the members of the group. Maximum length: 30 words. Lorem ipsum, dolor sit amet consectetur adipisicing elit repellat veniam eaque consequatur dolorem provident dolorum" },
+        
         { thumbnail: "project thumbnail from db.", github: "github link from db", website: "website link from db.", title: "Project Title from db.", description: "Project description from db.", members: "All the Team members from db." },
+        
         { thumbnail: "", github: "", website: "", title: "", description: "", members: "" }
       ],
     };
