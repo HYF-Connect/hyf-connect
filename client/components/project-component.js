23fbcdcb5d9bedbb0654ba5c9a222d12e9dce4c7
Dirk Jacobs <dirk.on.web@hotmail.com>
2022-01-08 15:17:52 +0100

    Another responsive layout option

--- client/components/project-component.js
+++ client/components/project-component.js
@@ -2,20 +2,22 @@ const ProjectComponent = {
   props: ['project'],
   template: `  
   <div class="project__component--container">
-    <div class="project__thumbnail"><img v-bind:src="project.thumbnail" alt="project thumbnail"></div>
+    <div class="project__thumbnail">
+      <img v-bind:src="project.thumbnail" alt="project thumbnail">
+    </div>
+    <div class="project__description">
+      <h4 class="project__title" >{{project.title}}</h4>
+      <p>{{project.description}}</p>
+      <span class="project__subtitle">Team members</span>
+      <p>{{project.members}}</p>
+    </div>
     <div class="project__buttons">
-      <div class="project__github--url">
+      <div class="project__button">
         <a v-bind:href="project.github">see project</a>
       </div>
-      <div class="project__website--url">
+      <div class="project__button">
         <a v-bind:href="project.website">visit website</a>
       </div>
-    </div>
-    <div class="project__component_description">
-      <h4 class="project__title" >{{project.title}}</h4>
-      <p class="project__description">{{project.description}}</p>
-      <span class="project__subtitle">team members:</span>
-      <p class="project__members">{{project.members}}</p>
     </div>
   </div>`
 }
