1b89219a346714a333e02b00674660876dcd5080
Dirk Jacobs <dirk.on.web@hotmail.com>
2022-01-08 13:21:15 +0100

    Try to implment with grid

--- client/components/project-component.js
+++ client/components/project-component.js
@@ -2,18 +2,16 @@ const ProjectComponent = {
   props: ['project'],
   template: `  
   <div class="project__component--container">
-    <div class="project__component--left">
-      <div class="project__thumbnail"><img v-bind:src="project.thumbnail" alt="project thumbnail"></div>
-      <div class="project__buttons">
-        <div class="project__github--url">
-          <a v-bind:href="project.github">see project</a>
-        </div>
-        <div class="project__website--url">
-          <a v-bind:href="project.website">visit website</a>
-        </div>
+    <div class="project__thumbnail"><img v-bind:src="project.thumbnail" alt="project thumbnail"></div>
+    <div class="project__buttons">
+      <div class="project__github--url">
+        <a v-bind:href="project.github">see project</a>
+      </div>
+      <div class="project__website--url">
+        <a v-bind:href="project.website">visit website</a>
       </div>
     </div>
-    <div class="project__component--right">
+    <div class="project__component_description">
       <h4 class="project__title" >{{project.title}}</h4>
       <p class="project__description">{{project.description}}</p>
       <span class="project__subtitle">team members:</span>
