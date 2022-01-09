eadb234e1d8345640a9dcc51379d4854f2340089
Dirk Jacobs <dirk.on.web@hotmail.com>
2022-01-09 19:48:31 +0100

    Split  and small update to footer

--- client/components/footer-component.js
+++ client/components/footer-component.js
@@ -1,20 +1,33 @@
 const FooterComponent = {
   template: `
-    <div id="footer">
-    <a class="footer_pp" href="/">
-    <span >Privacy Policy | Terms and Condition</span>
-    </a>
-    <a  href="/">
-    <span class="footer_connect">© HYF Connect 2021</span>
-    <img class="hyf_ico" alt="HYF_connect logo" src="/assets/logo_white_icon.png" width="78" height="78"/>
-    </a>
-    
-    <a  href="https://github.com/HYF-Connect">
-    <span class="footer_code">code along with us</span>
-    <img class="link_ico" alt="HYF_connect logo" src="/assets/logo-github.svg"/>
-    <img class="link_ico" alt="HYF_connect logo" src="/assets/logo-facebook.svg" />
-    <img class="link_ico" alt="HYF_connect logo" src="/assets/logo-instagram.svg"/>
-    </a>
+    <div class="footer">
+      <div class="footer_panel">
+      <a class="footer_text" href="/">
+        <span >Privacy Policy | Terms and Condition</span>
+      </a>
+      </div>
+      <div class="footer_panel">
+        <a class="footer_text" href="/">
+          <div class="footer_text">© HYF Connect 2021</div>
+        </a>
+        <a class="footer_text" href="/">
+          <img alt="HYF_connect logo" src="/assets/logo_white_icon.png" width="78" height="78"/>
+        </a>
+      </div>
+      <div class="footer_panel">
+        <div class="footer_text">code along with us</div>
+        <div class="footer_links">
+          <a  href="https://github.com/HYF-Connect" target="_blank">
+            <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-github.svg"/>
+          </a>
+          <a  href="https://github.com/HYF-Connect" target="_blank">
+          <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-facebook.svg" />
+          </a>
+          <a  href="https://github.com/HYF-Connect" target="_blank">
+          <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-instagram.svg"/>
+          </a>
+        </div>  
+      </div>    
     
     </div>`,
 };
