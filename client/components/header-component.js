a0b10aa5397bd2687bd2d560c7fdb33793a30dcf
Dirk Jacobs <dirk.on.web@hotmail.com>
2022-01-10 20:34:21 +0100

    Initial steps for resonsive menu

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -8,23 +8,38 @@ const HeaderComponent = {
       <div class="brand">
         <a href="/pages/homepage/homepage.html" class="logo"><img src="/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
       </div>
-      <div class="nav-bar d-none d-lg-block">
+      <div class="nav-bar header-menu-horizontal">
           <a class="nav-link" href="/pages/projects/projects.html">projects</a>
           <a class="nav-link" href="/pages/members/members.html">members</a>
           <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
           <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
       </div>
-      <div class="d-none d-lg-block">
+      <div class="header-menu-avatar">
         <avatar-dropdown :avatar="avatar" v-if="isLoggedIn"></avatar-dropdown>
         <a class="sign-in-btn" href="/pages/login/login.html" v-else>sign in</a>
       </div>
-      <div class="d-block d-lg-none">
-        <p>Burger comes here</p>
+      <div class="header-menu-burger">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+              data-bs-target="#header-menu-burger">
+          <span class="header-burger_line header-burger_line_first"></span>
+          <span class="header-burger_line header-burger_line_second"></span>
+          <span class="header-burger_line header-burger_line_third"></span>
+        </button>
+        <div id="header-menu-burger" class="collapse navbar-collapse header-menu-vertical">
+          <button class="navbar-toggler header-burger-close" type="button" data-bs-toggle="collapse" 
+               data-bs-target="#header-menu-burger">Close
+          </button>
+          <div class="">Menu comes here</div>
+          <div class="">Menu comes here</div>
+          <div class="">Menu comes here</div>
+          <div class="">Menu comes here</div>
+          <div class="">Menu comes here</div>
+          <div class="">Menu comes here</div>
+        </div >
       </div>
     </div>`,
   data() {
     let isLoggedIn = true;
-    //console.log(localStorage.getItem("token"));
     if (localStorage.getItem("token") == undefined) {
       isLoggedIn = false;
     }
