b803218ffd67aebc6e00c013b38dce571c4ae6fb
Dirk Jacobs <dirk.on.web@hotmail.com>
2022-01-10 16:56:15 +0100

    Add some responsiveness

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -1,19 +1,25 @@
 import AvatarDropdown from "./avatar-dropdown-component.js";
 const HeaderComponent = {
   components: {    AvatarDropdown,  },
-  template: `<div class="header-container">
-    <div class="brand">
+  template: `
+    <div class="header-container">
+      <div class="brand">
         <a href="/pages/homepage/homepage.html" class="logo"><img src="/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
-        </div>
-        <div class="nav-bar">
-            <a class="nav-link" href="/pages/projects/projects.html">projects</a>
-            <a class="nav-link" href="/pages/members/members.html">members</a>
-            <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
-            <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
-        </div>
+      </div>
+      <div class="nav-bar d-none d-lg-block">
+          <a class="nav-link" href="/pages/projects/projects.html">projects</a>
+          <a class="nav-link" href="/pages/members/members.html">members</a>
+          <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
+          <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
+      </div>
+      <div class="d-none d-lg-block">
         <avatar-dropdown :avatar="avatar" v-if="isLoggedIn"></avatar-dropdown>
         <a class="sign-in-btn" href="/pages/login/login.html" v-else>sign in</a>
-        </div>`,
+      </div>
+      <div class="d-block d-lg-none">
+        <p>Burger comes here</p>
+      </div>
+    </div>`,
   data() {
     let isLoggedIn = true;
     //console.log(localStorage.getItem("token"));
