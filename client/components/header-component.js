d7130580469354a8338821ec565c3c6986bec87c
reinaldo <perezrei@gmail.com>
2022-01-11 16:16:20 +0100

    create burger menu

--- client/components/header-component.js
+++ client/components/header-component.js
@@ -2,22 +2,43 @@ import AvatarDropdown from "./avatar-dropdown-component.js";
 import { fetchUserById } from "../src/data-access/api-calls/calls.js";
 
 const HeaderComponent = {
-  components: { AvatarDropdown },
-  template: `<div class="header-container">
-    <div class="brand">
-        <a href="/pages/homepage/homepage.html" class="logo">
-        <img src="/assets/logo_pc_full.png" alt="logo" width="90" height="70"/>
-        </a>
-        </div>
-        <div class="nav-bar">
-            <a class="nav-link" href="/pages/projects/projects.html">projects</a>
-            <a class="nav-link" href="/pages/members/members.html">members</a>
-            <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
-            <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
-        </div>
+  components: {    AvatarDropdown,  },
+  template: `
+    <div class="header-container">
+      <div class="brand">
+        <a href="/pages/homepage/homepage.html" class="logo"><img src="/assets/logo_pc_full.png" alt="logo" width="90" height="70"/></a>
+      </div>
+      <div class="nav-bar header-menu-horizontal">
+          <a class="nav-link" href="/pages/projects/projects.html">projects</a>
+          <a class="nav-link" href="/pages/members/members.html">members</a>
+          <a class="nav-link" href="/pages/about-us/about-us.html">about us</a>
+          <a class="nav-link" href="/pages/contact-us/contact-us.html">contact us</a>
+      </div>
+      <div class="header-menu-avatar">
         <avatar-dropdown :avatar="avatar" v-if="isLoggedIn"></avatar-dropdown>
         <a class="sign-in-btn" href="/pages/login/login.html" v-else>sign in</a>
-        </div>`,
+      </div>
+      <div class="header-menu-burger">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+              data-bs-target="#header-menu-burger">
+          <span class="header-burger_line header-burger_line_first"></span>
+          <span class="header-burger_line header-burger_line_second"></span>
+          <span class="header-burger_line header-burger_line_third"></span>
+        </button>
+        <div id="header-menu-burger" class="collapse navbar-collapse header-menu-vertical">
+          <button class="navbar-toggler header-burger-close" type="button" data-bs-toggle="collapse" 
+              data-bs-target="#header-menu-burger">
+            <span class="header-menu-close-line"></span>
+            <span class="header-menu-close-line"></span>
+          </button>
+          <div class="">projects</div>
+          <div class="">members</div>
+          <div class="">about us</div>
+          <div class="">contact us</div>
+          <div class="">sign in</div>
+        </div >
+      </div>
+    </div>`,
   data() {
     let isLoggedIn = true;
     //console.log(localStorage.getItem("token"));
