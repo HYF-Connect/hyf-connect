cf351f45cf71815baf0c410dc8f0a0144212c53b
Rayane Silva <rayanej.silva@gmail.com>
2022-01-17 00:23:38 +0100

    fixed text decoration and positioning

--- client/components/footer-component.js
+++ client/components/footer-component.js
@@ -1,34 +1,33 @@
 const FooterComponent = {
-  template: `
+   template: `
     <div class="footer">
-      <div class="footer_panel">
-      <a class="footer_text" href="/">
-        <span >Privacy Policy | Terms and Condition</span>
-      </a>
-      </div>
-      <div class="footer_panel">
+        <div class="footer_panel-terms">
         <a class="footer_text" href="/">
-          <div class="footer_text">© HYF Connect 2021</div>
+          <span >Privacy Policy | Terms and Condition</span>
         </a>
-        <a class="footer_text" href="/">
-          <img alt="HYF_connect logo" src="/assets/logo_white_icon.png" width="78" height="78"/>
-        </a>
-      </div>
-      <div class="footer_panel">
-        <div class="footer_text">code along with us</div>
-        <div class="footer_links">
-          <a  href="https://github.com/HYF-Connect" target="_blank">
-            <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-github.svg"/>
+        </div>
+        <div class="footer_panel">
+          <a class="footer_text" href="/">
+            <img alt="HYF_connect logo" src="/assets/logo_white_icon.png" width="78" height="78"/>
           </a>
-          <a  href="https://github.com/HYF-Connect" target="_blank">
-          <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-facebook.svg" />
+          <a class="footer_text" href="/">
+            <div class="footer_text-hyf">© HYF Connect 2022</div>
           </a>
-          <a  href="https://github.com/HYF-Connect" target="_blank">
-          <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-instagram.svg"/>
-          </a>
-        </div>  
-      </div>    
-    
+        </div>
+        <div class="footer_panel-icon">
+          <div class="footer_text">code along with us</div>
+          <div class="footer_links-icon">
+            <a  href="https://github.com/HYF-Connect" target="_blank">
+              <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-github.svg"/>
+            </a>
+            <a  href="https://github.com/HYF-Connect" target="_blank">
+            <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-facebook.svg" />
+            </a>
+            <a  href="https://github.com/HYF-Connect" target="_blank">
+            <img class="footer_link_ico" alt="HYF_connect logo" src="/assets/logo-instagram.svg"/>
+            </a>
+          </div>  
+        </div>
     </div>`,
 };
 
