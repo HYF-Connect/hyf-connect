f5aca32f7c96a3c123d0de785e615deae54ba351
Rayane Silva <rayanej.silva@gmail.com>
2022-01-18 16:41:03 +0100

    replaced the logo for a better quality

--- client/components/avatar-dropdown-component.js
+++ client/components/avatar-dropdown-component.js
@@ -12,7 +12,7 @@ export const AvatarDropdown = {
   </button>
   <ul class="dropdown-menu" id="bg-menu" aria-labelledby="dropdownMenu1" >
     <li><a class="dropdown-item" id="item" href="/pages/edit-user-profile/edit-user-profile.html">My profile</a></li>
-      <li><a class="dropdown-item"id="item" href="/pages/user-project/user-project.html">My projects</a></li>
+      <li><a class="dropdown-item" id="item" href="/pages/user-project/user-project.html">My projects</a></li>
       <li class="avatar-sign-out-btn-container"><a class="sign-out-btn" href="" v-on:click="logOut">sign out</a></li>
   </ul>
 </div>
